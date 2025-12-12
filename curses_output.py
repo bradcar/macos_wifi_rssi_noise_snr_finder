c8ac9896d28db4137f2d1fe3b493ab168b733f17
bradcar <bc@bradcarlile.com>
2025-12-11 22:57:47 -0800

    added description to curses_output.py

--- curses_output.py
+++ curses_output.py
@@ -36,11 +36,45 @@ def main_window(stdscr):
         snr = rssi - noise
         duration = time.time() - start_time
 
+        # RSSI quality, higher is better
+        if rssi > -50:
+            rssi_string = "4 bars"
+        elif rssi > -60:
+            rssi_string = "3 bars"
+        elif rssi > -70:
+            rssi_string = "2 bars"
+        elif rssi > -80:
+            rssi_string = "1 bar"
+        else:
+            rssi_string = "0 bar"
+
+        # Noise level, lower is better
+        if noise < -90:
+            noise_string = "very low"
+        elif noise > -80:
+            noise_string = "low"
+        elif noise > -70:
+            noise_string = "moderate"
+        elif noise > -60:
+            noise_string = "high"
+        else:
+            noise_string = "very high"
+
+        # SNR quality, higher is better
+        if snr > 30:
+            snr_string = "very good"
+        elif snr > 25:
+            snr_string = "good"
+        elif snr > 14:
+            snr_string = "ok"
+        else:
+            snr_string = "poor"
+
         stdscr.clear()
         stdscr.addstr(1, 1, f"WiFi Signal Monitor: {ssid}")
-        stdscr.addstr(3, 4, f"RSSI:  {rssi:>4} dBm")
-        stdscr.addstr(4, 4, f"Noise: {noise:>4} dBm")
-        stdscr.addstr(5, 4, f"SNR:   {snr:>4} dB")
+        stdscr.addstr(3, 4, f"RSSI: {rssi:>4} dBm  {rssi_string}")
+        stdscr.addstr(4, 4, f"Noise:{noise:>4} dBm  {noise_string}")
+        stdscr.addstr(5, 4, f"SNR:  {snr:>4} dB   {snr_string}")
 
         stdscr.addstr(7, 4, f"Updates:  {duration * 1000:.1f} msec, {1.0 / duration:.0f} Hz")
         stdscr.addstr(8, 4, f"Clock: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
